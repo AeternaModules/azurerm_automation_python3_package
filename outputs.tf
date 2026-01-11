a02a0dc437ca154e9c43c82823efc329ac9aa960
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:55:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_python3_packages" {
+  description = "All automation_python3_package resources"
+  value       = azurerm_automation_python3_package.automation_python3_packages
+}
+
